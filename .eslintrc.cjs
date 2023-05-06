09b47cbef0fd7a350fae1caebfe51706d5eb9b51
liuhuiyuan <13201479182@163.com>
2023-05-06 18:07:29 +0800

    :sparkles: ts: 语法规范约束

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,60 +3,76 @@
  */
 const commonRules = {
     // 将prettier作为插件使用,提示报警信息
-    "prettier/prettier": 1,
+    'prettier/prettier': 1,
 
-    "for-direction": 2,
-    "getter-return": 2,
-    "no-async-promise-executor": 2,
-    "no-compare-neg-zero": 2,
-    "no-cond-assign": 2,
-    "no-console": 1,
-    "no-constant-condition": 2,
-    "no-debugger": 1,
-    "no-dupe-keys": 2,
-    "no-duplicate-case": 2,
-    "no-empty": 2,
-    "no-func-assign": 2,
-    "no-inner-declarations": 2,
-    "no-implicit-coercion": 2,
-    "no-lone-blocks": 2,
-    "no-magic-numbers": 2,
-    "no-new-wrappers": 2,
-    "no-param-reassign": 2,
-    "no-proto": 2,
-    "no-script-url": 2,
-    "no-throw-literal": 2,
-    "no-useless-call": 2,
-    "no-useless-escape": 2,
-    "prefer-named-capture-group": 2,
-    "prefer-promise-reject-errors": 2,
-    "require-await": 2,
-    "require-unicode-regexp": 2,
-    "no-warning-comments": [
+    'for-direction': 2,
+    'getter-return': 2,
+    'no-async-promise-executor': 2,
+    'no-compare-neg-zero': 2,
+    'no-cond-assign': 2,
+    'no-console': 1,
+    'no-constant-condition': 2,
+    'no-debugger': 1,
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    'no-empty': 2,
+    'no-func-assign': 2,
+    'no-inner-declarations': 2,
+    'no-implicit-coercion': 2,
+    'no-lone-blocks': 2,
+    'no-magic-numbers': [
         2,
         {
-            terms: ["todo", "fix"],
-            location: "start",
+            ignoreArrayIndexes: true,
+            ignoreDefaultValues: true,
+            enforceConst: true,
+            ignoreClassFieldInitialValues: true,
         },
     ],
-    yoda: 2,
-    "no-shadow": 2,
-    camelcase: 2,
-    "consistent-this": [2, "self"],
-    "line-comment-position": 2,
-    "max-depth": [2, 4],
-    "max-lines": [2, 300],
-    "max-lines-per-function": [2, 30],
-    "max-statements-per-line": [
+    'no-new-wrappers': 2,
+    'no-param-reassign': 2,
+    'no-proto': 2,
+    'no-script-url': 2,
+    'no-throw-literal': 2,
+    'no-useless-call': 2,
+    'no-useless-escape': 2,
+    'prefer-named-capture-group': 2,
+    'prefer-promise-reject-errors': 2,
+    'require-await': 2,
+    'require-unicode-regexp': 2,
+    'no-warning-comments': [
+        2,
+        {
+            terms: ['todo', 'fix'],
+            location: 'start',
+        },
+    ],
+    'yoda': 2,
+    'camelcase': 2,
+    'consistent-this': [2, 'self'],
+    'line-comment-position': 2,
+    'max-depth': [2, 4],
+    'max-lines': [2, 300],
+    'max-lines-per-function': [2, 30],
+    'max-statements-per-line': [
         2,
         {
             max: 1,
         },
     ],
-    "new-cap": 2,
-    "no-mixed-operators": 2,
-    "constructor-super": 2,
-    "prefer-rest-params": 2,
+    'new-cap': 2,
+    'no-mixed-operators': [
+        2,
+        {
+            groups: [
+                ['&', '|', '^', '~', '<<', '>>', '>>>'],
+                ['&&', '||'],
+            ],
+            allowSamePrecedence: true,
+        },
+    ],
+    'constructor-super': 2,
+    'prefer-rest-params': 2,
 };
 
 /**
@@ -64,9 +80,10 @@ const commonRules = {
  * 针对js语法规则
  */
 const jsRules = {
-    "no-redeclare": 2,
-    "no-unused-vars": 2,
-    "no-use-before-define": 2,
+    'no-redeclare': 2,
+    'no-unused-vars': 2,
+    'no-use-before-define': 2,
+    'no-shadow': 2,
 };
 
 /**
@@ -74,7 +91,29 @@ const jsRules = {
  * 针对ts语法规则
  */
 const tsRules = {
-    "@typescript-eslint/adjacent-overload-signatures": 2,
+    '@typescript-eslint/adjacent-overload-signatures': 2,
+    '@typescript-eslint/array-type': [
+        2,
+        {
+            default: 'array',
+            readonly: 'array',
+        },
+    ],
+    '@typescript-eslint/await-thenable': 2,
+    '@typescript-eslint/ban-types': 2,
+    '@typescript-eslint/class-literal-property-style': [2, 'fields'],
+    '@typescript-eslint/consistent-generic-constructors': [2, 'constructor'],
+    '@typescript-eslint/consistent-indexed-object-style': [
+        2,
+        'index-signature',
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+        2,
+        {
+            assertionStyle: 'as',
+            objectLiteralTypeAssertions: 'allow-as-parameter',
+        },
+    ],
 };
 
 module.exports = {
@@ -83,25 +122,25 @@ module.exports = {
     overrides: [
         // js & cjs 文件处理
         {
-            files: ["*.js", "*.cjs"],
+            files: ['*.js', '*.cjs'],
             rules: Object.assign({}, commonRules, jsRules),
             parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
+                ecmaVersion: 'latest',
+                sourceType: 'module',
             },
-            plugins: ["prettier"]
+            plugins: ['prettier'],
         },
 
         // ts 文件处理
         {
-            files: ["*.ts"],
+            files: ['*.ts'],
             rules: Object.assign({}, commonRules, tsRules),
-            parser: "@typescript-eslint/parser",
+            parser: '@typescript-eslint/parser',
             parserOptions: {
-                ecmaVersion: "latest",
-                project: './tsconfig.json'
+                ecmaVersion: 'latest',
+                project: './tsconfig.json',
             },
-            plugins: ["@typescript-eslint", "prettier"],
+            plugins: ['@typescript-eslint', 'prettier'],
         },
     ],
 };
--- eslint-rule-test/eslint.ts
+++ eslint-rule-test/eslint.ts
@@ -1,8 +1,72 @@
 /**
- * 1. @typescript-eslint/adjacent-overload-signatures
+ * 1. adjacent-overload-signatures
  * 强制函数重载及其其实现放在一起
  */
 // export function foo(s: string): string;
 // export function bar(d: boolean): boolean;
 // export function foo(n: number): number;
 // export function foo(arg: any): any {}
+
+/**
+ * 2. array-type
+ * 强制数组始终使用T[] | readonly T[]
+ */
+// const x: Array<string> = ['1'];
+// const y: ReadonlyArray<number> = [1];
+// const z: string[] = ['1'];
+// const w: readonly number[] = [1, 3];
+
+/**
+ * 3. await-thenable
+ * 规避不正当的使用await
+ */
+// const a = () => {};
+// await a();
+
+/**
+ * 4. ban-types
+ * 禁止使用内置类型充当数据的类型
+ */
+// const str: String = 'foo';
+// const bool: Boolean = true;
+// const num: Number = 1;
+// const symb: Symbol = Symbol('foo');
+// const lowerObj: Object = {};
+// const curly: {} = 1;
+
+/**
+ * 5. class-literal-property-style
+ * 规定所有getter方法都使用修饰符型的字段来定义他们
+ */
+// class People {
+//     private readonly age = 100;
+//     public get age2() {
+//         // eslint-disable-next-line no-magic-numbers
+//         return 100;
+//     }
+// }
+
+/**
+ * 6. consistent-generic-constructors
+ * 规定所有的泛型构造函数泛型变量都出现在构造函数上
+ */
+// const map: Map<string, number> = new Map();
+// const map = new Map<string, number>();
+
+/**
+ * 7. consistent-indexed-object-style
+ * 规定所有的索引签名采用接口 而非Record
+ */
+// type Foo = Record<string, unknown>;
+// interface Foo {
+//     [key: string]: number;
+// }
+
+/**
+ * 8. consistent-type-assertions
+ * 规定采用一致的断言, as&类型前置,除了const any unknown
+ */
+// const y = {} as any;
+// const z = {} as unknown;
+// const x = {} as const;
+// const d = {} as number;
